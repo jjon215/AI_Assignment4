18baed880c7748b90fb6b83bad35823a3bab44ab
jessjon <Pottan123*>
2018-12-03 21:38:47 -0500

    cross entropy implementation

--- Test.py
+++ Test.py
@@ -1,55 +1,59 @@
+
 import gym
+gym.logger.set_level(40) # suppress warnings (please remove if gives error)
 import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
 
-env = gym.make('CartPole-v0')
-print('observation space:', env.observation_space)
-print('action space:', env.action_space)
 
-
-class Policy():
-    def __init__(self, s_size=4, a_size=2):
-        self.w = 1e-4 * np.random.rand(s_size, a_size)  # weights for simple linear policy: state_space x action_space
-
-    def forward(self, state):
-        x = np.dot(state, self.w)
-        return np.exp(x) / sum(np.exp(x))
-
-    def act(self, state):
-        probs = self.forward(state)
-        # action = np.random.choice(2, p=probs) # option 1: stochastic policy
-        action = np.argmax(probs)  # option 2: deterministic policy
-        return action
-
+import torch
+torch.manual_seed(0) # set random seed
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
 
 env = gym.make('CartPole-v0')
 env.seed(0)
-np.random.seed(0)
+print('observation space:', env.observation_space)
+print('action space:', env.action_space)
 
-policy = Policy()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def hill_climbing(n_episodes=1000, max_t=1000, gamma=1.0, print_every=100, noise_scale=1e-2):
-    """Implementation of hill climbing with adaptive noise scaling.
+class Policy(nn.Module):
+    def __init__(self, s_size=4, h_size=16, a_size=2):
+        super(Policy, self).__init__()
+        self.fc1 = nn.Linear(s_size, h_size)
+        self.fc2 = nn.Linear(h_size, a_size)
 
-    Params
-    ======
-        n_episodes (int): maximum number of training episodes
-        max_t (int): maximum number of timesteps per episode
-        gamma (float): discount rate
-        print_every (int): how often to print average score (over last 100 episodes)
-        noise_scale (float): standard deviation of additive noise
-    """
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+
+    def act(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        probs = self.forward(state).cpu()
+        m = Categorical(probs)
+        action = m.sample()
+        return action.item(), m.log_prob(action)
+
+
+policy = Policy().to(device)
+optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+
+
+def reinforce(n_episodes=1000, max_t=1000, gamma=1.0, print_every=100):
     scores_deque = deque(maxlen=100)
     scores = []
-    best_R = -np.Inf
-    best_w = policy.w
     for i_episode in range(1, n_episodes + 1):
+        saved_log_probs = []
         rewards = []
         state = env.reset()
         for t in range(max_t):
-            action = policy.act(state)
+            action, log_prob = policy.act(state)
+            saved_log_probs.append(log_prob)
             state, reward, done, _ = env.step(action)
             rewards.append(reward)
             if done:
@@ -60,34 +64,29 @@ def hill_climbing(n_episodes=1000, max_t=1000, gamma=1.0, print_every=100, noise
         discounts = [gamma ** i for i in range(len(rewards) + 1)]
         R = sum([a * b for a, b in zip(discounts, rewards)])
 
-        if R >= best_R:  # found better weights
-            best_R = R
-            best_w = policy.w
-            noise_scale = max(1e-3, noise_scale / 2)
-            policy.w += noise_scale * np.random.rand(*policy.w.shape)
-        else:  # did not find better weights
-            noise_scale = min(2, noise_scale * 2)
-            policy.w = best_w + noise_scale * np.random.rand(*policy.w.shape)
+        policy_loss = []
+        for log_prob in saved_log_probs:
+            policy_loss.append(-log_prob * R)
+        policy_loss = torch.cat(policy_loss).sum()
+
+        optimizer.zero_grad()
+        policy_loss.backward()
+        optimizer.step()
 
         if i_episode % print_every == 0:
             print('Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
         if np.mean(scores_deque) >= 195.0:
             print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
                                                                                        np.mean(scores_deque)))
-            policy.w = best_w
             break
 
     return scores
 
 
-scores = hill_climbing()
-
-
-env = gym.make('CartPole-v0')
-state = env.reset()
-for t in range(200):
-    action = policy.act(state)
-    env.render()
-    state, reward, done, _ = env.step(action)
-    if done:
-        break
+scores = reinforce()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
